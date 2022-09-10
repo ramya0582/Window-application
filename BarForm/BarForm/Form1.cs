dde451e580c51566ecd7c56269a903b406c817bc
ramya0582 <101280628+ramya0582@users.noreply.github.com>
2022-09-10 22:30:19 +0530

    Add files via upload

---
+++ BarForm/BarForm/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"server=DESKTOP-C9VA90N; Initial Catalog = RamRelations; User ID = sa; Password = ramya");
+
+            SqlCommand cmd = new SqlCommand("select * from Login where UserName=@UserName and Password=@Password", con);
+
+            cmd.Parameters.AddWithValue("@UserName", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Password", txtpassword.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            //Connection open here   
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Successfully loged in");
+               
+            }
+            else
+            {
+                MessageBox.Show("Please enter Correct Username and Password");
+            }
+        }
+    }
+}
+
+       
